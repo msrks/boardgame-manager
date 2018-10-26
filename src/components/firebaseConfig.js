71f5e368568f4cd07073d2ed7c091e0c5dfdee6e
msrks <mshrrks@gmail.com>
2018-10-26 17:03:21 +0900

    add firestore

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCo2a7N4eWt4i0QadbEhhnxbOfwL1DogyE",
+  authDomain: "boardgame-manager-2018.firebaseapp.com",
+  databaseURL: "https://boardgame-manager-2018.firebaseio.com",
+  projectId: "boardgame-manager-2018",
+  storageBucket: "boardgame-manager-2018.appspot.com",
+  messagingSenderId: "473964914862"
+}
